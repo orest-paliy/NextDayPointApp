fb7dd5db4efbc7d7f908238daac5d4e5d7fa5eff
Orest Palii <paliy.o.2004@gmail.com>
2025-10-04 14:30:36 +0300

    Initial commit

---
+++ NextDayPointApp/NextDayPointAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NextDayPointAppApp.swift
+//  NextDayPointApp
+//
+//  Created by Orest Palii on 04.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct NextDayPointAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
